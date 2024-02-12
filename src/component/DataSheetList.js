794090b63a816b0dae3de8aa5d5585c52be1a073
Loganathan <loganathanvelumani2300@gmail.com>
2024-02-12 15:59:41 +0530

    rework on tabledata

--- src/component/DataSheetList.js
+++ src/component/DataSheetList.js
@@ -14,17 +14,20 @@ export const DataSheetList = () => {
       street: item.address.street,
       suite: item.address.suite,
       city: item.address.city,
+      zipcode:item.address.zipcode,
+      // lat: item.geo.lat,
+      // lng:item.geo.lng,
     }));
 
     // Set the transformed data into the state variable
     setData(transformedData);
   }, []); // Empty dependency array ensures this effect runs only once after the initial render
 
-  const tableColumns = ["name", "username", "email", "street", "suite", "city"];
+  const tableColumns = ["name", "username", "email", "street", "suite", "city","zipcode"];
 
   return (
-    <div className="m-5">
-      <div className="card py-lg-5 pb-lg-0 pt-lg-4 py-sm-3">
+    <div className="m-5 mt-0 mx-0">
+      <div className="card position-fixed py-lg-5 pb-lg-0 pt-lg-4 py-sm-3 mt-0 m-3">
         <TableData tableHeader={tableColumns} tableDatas={data} />
       </div>
     </div>
